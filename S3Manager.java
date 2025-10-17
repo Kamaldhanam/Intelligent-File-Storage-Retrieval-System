65d1e661b9cac79e7e310639f25aa82193f6077c
Kamal P <kamaldhanam888@gmail.com>
2025-10-17 20:38:24 +0530

    Create S3Manager.java

---
+++ S3Manager.java
@@ -0,0 +1,32 @@
+package filestorage;
+
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.File;
+
+public class S3Manager {
+    private static final String BUCKET_NAME = "your-s3-bucket-name";
+
+    public static void uploadFile(String filePath, String s3Key) {
+        try {
+            S3Client s3 = S3Client.builder()
+                    .region(Region.US_EAST_1)
+                    .credentialsProvider(ProfileCredentialsProvider.create())
+                    .build();
+
+            File file = new File(filePath);
+            PutObjectRequest request = PutObjectRequest.builder()
+                    .bucket(BUCKET_NAME)
+                    .key(s3Key)
+                    .build();
+
+            s3.putObject(request, file.toPath());
+            System.out.println("Uploaded file to S3: " + s3Key);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
